416788a09307b618f208e17b894b19768a6b2a1e
Gregor Laan <laangregor@gmail.com>
2021-10-02 19:22:31 +0300

    Add route for quote component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuoteComponent } from './quote/quote.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: QuoteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
